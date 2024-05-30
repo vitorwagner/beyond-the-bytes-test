f39e273105c405d3649f3bdf2b2273b39f1fd123
Vitor Wagner <vitormwagner@gmail.com>
2024-05-30 19:52:31 -0300

    refactor: Update header links in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,14 @@ const Header = () => {
       <nav className="flex items-center justify-between p-6 lg:px-8">
         <h1 className="text-2xl font-bold">Mars Book</h1>        
         <div className="lg:flex lg:gap-x-12">
-          <span className="text-sm font-semibold leading-6 text-gray-900 p-2">
+          <span className="text-sm font-semibold text-gray-900 p-2">
             <Link to="/">Home</Link>
           </span>
-          <span className="text-sm font-semibold leading-6 text-gray-900 p-2">
-            <Link to="/create">Create</Link>
+          <span className="text-sm font-semibold text-gray-900 p-2">
+            <Link to="/create">Add Address</Link>
           </span>
         </div>
-        <span className="text-sm font-semibold leading-6 text-gray-400 p-2">Developed for Beyond the Bytes</span>
+        <span className="text-xs font-semibold text-gray-400 p-2">Developed for Beyond the Bytes</span>
       </nav>
     </header>
   );
