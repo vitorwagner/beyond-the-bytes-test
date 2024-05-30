ff89fd561065578b7a5ebd60563ecf9e142f34d7
Vitor Wagner <vitormwagner@gmail.com>
2024-05-30 18:32:16 -0300

    feat: Improved header and home page layout and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,20 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
+    <header>
+      <nav className="flex items-center justify-between p-6 lg:px-8">
+        <h1 className="text-2xl font-bold">Mars Book</h1>        
+        <div className="lg:flex lg:gap-x-12">
+          <span className="text-sm font-semibold leading-6 text-gray-900 p-2">
             <Link to="/">Home</Link>
-          </li>
-          <li>
+          </span>
+          <span className="text-sm font-semibold leading-6 text-gray-900 p-2">
             <Link to="/create">Create</Link>
-          </li>
-        </ul>
+          </span>
+        </div>
+        <span className="text-sm font-semibold leading-6 text-gray-400 p-2">Developed for Beyond the Bytes</span>
       </nav>
-    </div>
+    </header>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,25 +6,67 @@ import { UpdateProps } from '../types/addressUpdate';
 const Home: React.FC<UpdateProps> = ({ addressArray, setAddressArray }) => {
   const deleteAddress = (id: number) => {
     setAddressArray(addressArray.filter((address) => address.id !== id));
-  }
+  };
 
   return (
     <div>
       <Header />
-      <h1 className="text-3xl font-bold underline">Home</h1>
-
-      <ul>
+      <div className="mt-6 border-t border-gray-100 grid justify-items-center bg-slate-100">
         {addressArray.map((address) => (
-          <li key={address.id}>
-            <h2>{address.name}</h2>
-            <p>{address.location}</p>
-            <p>{address.phone}</p>
-            <p>{address.email}</p>
-            <Link to={`/update/${address.id}`}>Update</Link>
-            <button onClick={() => deleteAddress(address.id)}>Delete</button>
-          </li>
+          <dl className="divide-y divide-gray-100 border-2 rounded-md m-2 p-4 bg-white" key={address.id}>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Name
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {address.name}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Location
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {address.location}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Phone
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {address.phone}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Email
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {address.email}
+              </dd>
+            </div>
+            <img src="https://images.pexels.com/photos/269077/pexels-photo-269077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Building image"
+            className='w-full h-48 object-cover object-center rounded-md mt-4'
+            />
+            <div className="flex justify-between p-6 lg:px-8">
+              <button
+                type="button"
+                className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              >
+                <Link to={`/update/${address.id}`}>Update</Link>
+              </button>
+              <button
+                onClick={() => deleteAddress(address.id)}
+                type="button"
+                className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              >
+                Delete
+              </button>
+            </div>
+          </dl>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
